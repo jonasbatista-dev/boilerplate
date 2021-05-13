2363cbff3895365916a383c6b8b5c8c3d29d7eea
jonas batista <jonassilvat.i@hotmail.com>
2021-05-13 13:42:20 -0300

    new components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import * as S from './styles'
 
 const Main: React.FC = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="logo.svg" alt="Imagem de um átomo e React Avançado escrito ao lado" />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>Typescript, ReactJS, NextJS e Styled Components</S.Description>
+      <S.Ilustration src="hero-illustration.svg" alt="Um desenvolvedor cde frente para uma tela com código" />
+    </S.Wrapper>
   )
 };
 
